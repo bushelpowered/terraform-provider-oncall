0218c3aff6fa35bffec3ccd77763c3fd114240be
Oliver Isaac <oisaac@gmail.com>
2021-03-08 14:36:19 -0600

    added .github actinos

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.15
 require (
 	github.com/bushelpowered/oncall-client-go/oncall v0.0.0
 	github.com/hashicorp/go-cty v1.4.1-0.20200414143053-d3edf31b6320
+	github.com/hashicorp/terraform-plugin-docs v0.4.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.4.4
 	github.com/pkg/errors v0.9.1
+	github.com/zclconf/go-cty v1.7.1 // indirect
 	maze.io/x/duration v0.0.0-20160924141736-faac084b6075
 )
 
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package main // import "github.com/bushelpowered/terraform-provider-oncall"
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -6,6 +6,16 @@ import (
 
 	"github.com/bushelpowered/terraform-provider-oncall/oncall"
 )
+
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./examples/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
